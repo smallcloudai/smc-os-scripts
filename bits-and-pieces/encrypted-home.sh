8abe2b6576a5fe0121a1ef82f4a9344dbcf47d84
Oleg Klimov <omgtech@gmail.com>
2022-01-23 20:00:58 +0100

    Initial.

---
+++ bits-and-pieces/encrypted-home.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -xe
+throwawaykey=`openssl rand -hex 32`
+dmsetup create throwawaykey --table "0 `blockdev --getsz /dev/vdb` crypt aes-cbc-essiv:sha256 $throwawaykey 0 /dev/vdb 0"
+#                                                                        <cipher> <key> <iv_offset> <device path> <offset> [<#opt_params> <opt_params>]
+mkfs.ext4 /dev/mapper/throwawaykey
+tar -czf /tmp/homes.tar.gz /home
+mount /dev/mapper/throwawaykey /home
+tar -xzf /tmp/homes.tar.gz -C /
+echo "Successfully mounted encrypted home"
+
+# To destroy data:
+# umount /home; dmsetup remove_all --force; lsblk
