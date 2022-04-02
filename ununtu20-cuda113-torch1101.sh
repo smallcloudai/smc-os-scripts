a10f8e120073d1c8a044fed9f771158043d19478
Oleg Klimov <omgtech@gmail.com>
2022-04-02 08:36:07 +0200

    update automated

--- ununtu20-cuda113-torch1101.sh
+++ ununtu20-cuda113-torch1101.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-. bits-and-pieces/03-apt-install-after-nvidia
-. bits-and-pieces/05-ubuntu20-remove-unnecessary
+. bits-and-pieces/02-cuda
+. bits-and-pieces/05-llvm
+. bits-and-pieces/08-apt-install-after-nvidia
+. bits-and-pieces/10-ubuntu20-remove-unnecessary
 . bits-and-pieces/20-miniconda-py38
 . bits-and-pieces/25-pytorch
 . bits-and-pieces/30-pip-install
