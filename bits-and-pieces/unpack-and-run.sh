339cda73fe30d8f4abc275d4b6575c16a3028973
Oleg Klimov <omgtech@gmail.com>
2022-04-03 15:41:26 +0200

    deps 5

--- bits-and-pieces/unpack-and-run.sh
+++ bits-and-pieces/unpack-and-run.sh
@@ -5,5 +5,5 @@ if [ -f unpack_code.py ]; then
     sudo --user=user --set-home --login /usr/local/miniconda/bin/python unpack_code.py
 fi
 if [ -f run_task.py ]; then
-    sudo --user=user --set-home --login --background bash -c "nohup 2>&1 /usr/local/miniconda/bin/python run_task.py | tee --append ~/output.log"
+    sudo --user=user --set-home --login --background bash -c "nohup 2>&1 /usr/local/miniconda/bin/python run_task.py | tee --append /home/user/output.log"
 fi
